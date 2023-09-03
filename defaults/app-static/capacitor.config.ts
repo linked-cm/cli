4a564dac510b78983a5260f0b798f7c4b278e17d
René Verheij <email@rene.life>
2023-09-03 09:58:40 +0800

    capacitor default config - easier to start

--- defaults/app-static/capacitor.config.ts
+++ defaults/app-static/capacitor.config.ts
@@ -1,10 +1,48 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import {CapacitorConfig} from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.mydomain.FILL_IN_YOUR_OWN',
   appName: 'FILL_IN_YOUR_OWN',
   webDir: 'frontend/web',
-  bundledWebRuntime: false
+  server: {
+    allowNavigation: ['yourdomain.com', 'sub.yourdomain.com'],
+  },
+  bundledWebRuntime: false,
+  //Add or remove plugins that you need. Here is a starter list
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+    // FirebaseMessaging: {
+    //   presentationOptions: ['badge', 'sound', 'alert'],
+    // },
+    // GoogleAuth: {
+    //   scopes: ['profile', 'email'],
+    //   //self created production web client
+    //   clientId: '...',
+    //   //For these lines use the auto generated web client "Web client  (auto created by Google Service)"
+    //   serverClientId: '....',
+    //   androidClientId: '...',
+    //   iosClientId: '...',
+    //   forceCodeForRefreshToken: true,
+    // },
+    // SplashScreen: {
+    //   launchShowDuration: 400,
+    //   launchAutoHide: true,
+    //   backgroundColor: '#F7E5FF',
+    //   androidSplashResourceName: 'splash',
+    //   showSpinner: false,
+    //   splashFullScreen: true,
+    //   splashImmersive: true,
+    //   // launchFadeOutDuration: 500,
+    //   // androidScaleType: 'CENTER',
+    //   // androidSpinnerStyle: 'large',
+    //   // iosSpinnerStyle: 'small',
+    //   // spinnerColor: '#999999',
+    //   // layoutName: 'launch_screen',
+    //   // useDialog: true,
+    // },
+  },
 };
 
 export default config;
