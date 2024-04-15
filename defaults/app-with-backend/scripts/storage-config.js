2e9e3c12ca14a0dc9d9a8e2f6b27a753eda71385
René Verheij <email@rene.life>
2024-04-15 08:03:54 +0800

    rename variable

--- defaults/app-with-backend/scripts/storage-config.js
+++ defaults/app-with-backend/scripts/storage-config.js
@@ -2,5 +2,5 @@ const { NodeFileStore } = require('lincd-server/lib/shapes/NodeFileStore');
 const { LinkedStorage } = require('lincd/utils/LinkedStorage');
 
 //on the backend, we use a file store which stores all data as JSON-LD
-let fileStore = new NodeFileStore(process.env.NODE_ENV + '-main');
-LinkedStorage.setDefaultStore(fileStore);
+let quadStore = new NodeFileStore(process.env.NODE_ENV + '-main');
+LinkedStorage.setDefaultStore(quadStore);
